f9a3196a657b70e883ca0d4a8fb783a53ee70f9f
ain2108 <ain2108@columbia.edu>
2021-04-13 23:24:42 -0400

    fixed README and added some comments

--- counter.v
+++ counter.v
@@ -1,19 +1,25 @@
 module counter (
 
+    // 1.
     // wire is a wire hehe. What this means is it cannot store value
     // and therefore you cannot write to a wire. You can however connect
     // wires together.
     input wire clk,    // clock signal
     input wire rst,  // reset signal 
 
+    // 2. 
     // This output is coming from a register. You can think of it, in this case, 
     // as 8 flip flops each storing a bit. Thus you can indeed write into registers
     // on every clock cycle.
     output reg [7:0] out     // module's output signal
 );
 
+    // 3.
     // Always block gets executed whenever the signals in its "sensitivity" list change
     always @(posedge clk or posedge rst) begin
+        // 4.
+        // This looks like an if statement from Python ... it is not!
+        // Think how would you make an if statement in hardware.
         if (rst) begin
             out <= 0; // reset the counter on reset by writing 0
         end else begin
